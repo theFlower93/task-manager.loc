4c0acf9200e7b6f8d13496efbb9c981be644b3f1
theFlower93 <tsvetkov.dima93@gmail.com>
2026-02-05 23:39:56 +0500

    Политики (Policies)

---
+++ app/Policies/TaskPolicy.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Task;
+use App\Models\User;
+
+class TaskPolicy
+{
+    public function viewAny(User $user): bool
+    {
+        return true;
+    }
+
+    public function view(User $user, Task $task): bool
+    {
+        return $user->id === $task->user_id || $user->is_admin;
+    }
+
+    public function create(User $user): bool
+    {
+        return true;
+    }
+
+    public function update(User $user, Task $task): bool
+    {
+        return $user->id === $task->user_id;
+    }
+
+    public function delete(User $user, Task $task): bool
+    {
+        return $user->id === $task->user_id;
+    }
+
+    public function restore(User $user, Task $task): bool
+    {
+        return $user->id === $task->user_id;
+    }
+
+    public function forceDelete(User $user, Task $task): bool
+    {
+        return $user->is_admin;
+    }
+}
